bf1bfd9868ad11fd92751778d5c7124f082aa7eb
Cyrilsou <96681831+Cyrilsou@users.noreply.github.com>
2022-10-15 21:09:40 +0200

    Update Oasis.py

--- Oasis.py
+++ Oasis.py
@@ -1,7 +1,11 @@
 import requests
+import resp
+from resp import *
 from requests import *
 import pystyle
 from pystyle import *
+import time
+from time import *
 
 print(Colorate.Horizontal(Colors.blue_to_green, """
  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄ 
@@ -18,7 +22,8 @@ print(Colorate.Horizontal(Colors.blue_to_green, """
 
 """, 1))
 
-Select = int(input(Colorate.Horizontal(Colors.yellow_to_red, "[1] Webhook Spammer\n[2] Soon\n\n-->" , 1)))
+Select = int(input(Colorate.Horizontal(Colors.yellow_to_red, "[1] Webhook Spammer\n[2] Soon\n\n-->", 1)))
+
 
 def Webhook():
     Webhooks = input(Colorate.Horizontal(Colors.yellow_to_red, "Please enter Webhook -> ", 1))
@@ -32,19 +37,23 @@ def Webhook():
                 data = {"content": "Enter your text"}
                 r = post(Webhook, headers=headers, json=data)
                 print(r.text)
+                print(data)
+
 
         else:
             print(Colorate.Horizontal(Colors.yellow_to_red, "Please retry", 1))
             break
 
 
+if resp == {
+    "global": False,
+    "message": "The resource is being rate limited.",
+    "retry_after": {0}}:
+    sleep(10)
+    Webhook()
+    
+
 if Select == 1:
     Webhook()
 if Select == 2:
     None
-
-
-
-
-
-
